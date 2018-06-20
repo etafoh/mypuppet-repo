f7ef69c48e49f19f6e63bad4f1c773dece40769a
Elvis Tafoh <etafoh@gmail.com>
2018-06-20 22:04:35 +0100

    hello file added to tmp

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -4,6 +4,11 @@ file { '/usr/local/bin/run-puppet':
   mode   => '0755',
 }
 
+file {'/tmp/helloSir.txt':
+  ensure => file,
+  content => "This is a test file",
+}
+
 cron { 'run-puppet':
   command => '/usr/local/bin/run-puppet',
   hour    => '*',
